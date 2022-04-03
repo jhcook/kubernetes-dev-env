f53b7e51cf36ed79fa65813774cc8b2b51b06521
Justin Cook <jhcook@secnix.com>
2022-04-03 02:10:32 +0100

    major changes with using Rancher prometheus if available

--- calico_enterprise/README.md
+++ calico_enterprise/README.md
@@ -10,8 +10,36 @@ the following file names:
 
 Git is configured to ignore these filenames. 
 
-Execute `calico_enterprise/install_calico_enterprise.sh`
+Execute `calico_enterprise/install_calico_enterprise.sh` and after some time,
+you will be prompted to open a window to the UI:
 
+```
+Visit https://localhost:9443/ to login to the Calico Enterprise UI with token above.
+
+Forwarding from 127.0.0.1:9443 -> 9443
+Forwarding from [::1]:9443 -> 9443
+```
+
+## Prometheus
+
+If Rancher is installed with monitoring, the Calico Enterprise installation
+will use the Rancher Prometheus operator to manage the AlertManager and
+Prometheus CRs in the `tigera-prometheus` namespace.
+
+If no Prometheus operator exists, or is in an unknown namespace, the pull
+secret and operator patch will not be successful.
+
+If other Prometheus instances will need to be deployed alongside Tigera, then
+modification to the operator args will be necessary. By default, they are the
+following:
+
+```
+      - args:
+        - --prometheus-config-reloader=quay.io/tigera/prometheus-config-reloader:v3.13.0
+        - --config-reloader-memory-request=25Mi
+        - --namespaces=tigera-prometheus
+        - --cluster-domain=cluster.local
+```
 ## Post Configuration
 
 
@@ -20,3 +48,4 @@ Execute `calico_enterprise/install_calico_enterprise.sh`
 * [Calico Enterprise Installation](https://docs.tigera.io/getting-started/kubernetes/generic-install)
 * [Calico Resources](https://docs.tigera.io/reference/resources/)
 * [Configure access to Calico Enterprise Manager UI](https://docs.tigera.io/getting-started/cnx/access-the-manager)
+* [Prometheus Support](https://docs.tigera.io/maintenance/monitor/support)
--- calico_enterprise/install_calico_enterprise.sh
+++ calico_enterprise/install_calico_enterprise.sh
@@ -13,12 +13,19 @@
 #  * https://docs.tigera.io/maintenance/monitor/support
 #  * https://minikube.sigs.k8s.io/docs/handbook/persistent_volumes/
 #
+# Tested on:
+#  * macOS Montery 
+#
 # Author: Justin Cook
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 _NS_="tigera-operator"
 
 # Remove master taint
-kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/master- || /usr/bin/true
 
 # Create persistent volumes
 for i in {1..5}
@@ -59,32 +66,56 @@ kubectl delete APIServer default -n "${_NS_}"
 
 # Install the pull secret
 kubectl create secret generic tigera-pull-secret \
-    --type=kubernetes.io/dockerconfigjson -n "${_NS_}" \
-    --from-file=.dockerconfigjson=calico_enterprise/tigera-pull-secret.json \
-    --dry-run=client -o yaml | kubectl apply -f -
+  --type=kubernetes.io/dockerconfigjson -n "${_NS_}" \
+  --from-file=.dockerconfigjson=calico_enterprise/tigera-pull-secret.json \
+  --dry-run=client -o yaml | kubectl apply -f -
 
-# Create a pull secret for the Tigera Prometheus operator and patch deployment
-kubectl apply -f https://docs.tigera.io/manifests/tigera-prometheus-operator.yaml
+# Create a pull secret for the Tigera Prometheus operator and patch deployment.
+# In order to be idempotent, we need to be pedantic checking promtheuses and
+# known namespaces as prometheus API not available will change at a later date
+# and assumptions will become invalid.
+if ! kubectl get prometheus >/dev/null 2>&1
+then
+  PROMOPNS="tigera-prometheus"
+  PROMOPDP="calico-prometheus-operator"
+  kubectl apply -f https://docs.tigera.io/manifests/tigera-prometheus-operator.yaml
+else
+  if kubectl get ns cattle-monitoring-system >/dev/null 2>&1
+  then
+    PROMOPNS="cattle-monitoring-system"
+    PROMOPDP="rancher-monitoring-operator"
+  else
+    PROMOPNS="tigera-prometheus"
+    PROMOPDP="calico-prometheus-operator"
+  fi
+  kubectl create ns tigera-prometheus --dry-run=client -o yaml | \
+    kubectl apply -f -
+fi
+
 kubectl create secret generic tigera-pull-secret \
-    --type=kubernetes.io/dockerconfigjson -n tigera-prometheus \
-    --from-file=.dockerconfigjson=calico_enterprise/tigera-pull-secret.json \
-    --dry-run=client -o yaml | kubectl apply -f -
-kubectl patch deployment -n tigera-prometheus calico-prometheus-operator \
-    -p '{"spec":{"template":{"spec":{"imagePullSecrets":[{"name": "tigera-pull-secret"}]}}}}'
+  --type=kubernetes.io/dockerconfigjson -n ${PROMOPNS} \
+  --from-file=.dockerconfigjson=calico_enterprise/tigera-pull-secret.json \
+  --dry-run=client -o yaml | kubectl apply -f -
+kubectl patch deployment -n ${PROMOPNS} ${PROMOPDP} \
+  -p '{"spec":{"template":{"spec":{"imagePullSecrets":[{"name": "tigera-pull-secret"}]}}}}'
 
 # Install Tigera custom resources
 kubectl apply -f https://docs.tigera.io/manifests/custom-resources.yaml
 
-# Wait until apiserver and calico are Available. In order to circumvent
+# Helper function to verify tigerastatus is available. In order to circumvent
 # flapping, get three consecutive success to proceed.
-for serv in calico apiserver
-do
-  printf "Waiting on %s: " "${serv}"
+tigerastatus() {
+  # Wait for tigerastatuses.operator.tigera.io API group and kind
+  while :
+  do
+    kubectl get tigerastatus "$1" >/dev/null 2>&1 && break
+    sleep 2
+  done
   success_count=0
   until [ "$success_count" -gt 2 ]
   do
-    status="$(kubectl get tigerastatus ${serv} --no-headers 2>&1 | awk '{print$2}')"
-    if [ "${status}" == "True" ]
+    status="$(kubectl get tigerastatus $1 --no-headers | awk '{print$2}')"
+    if [ "${status:-False}" == "True" ]
     then
       ((success_count++))
     else
@@ -92,6 +123,13 @@ do
     fi
     sleep 2
   done
+}
+
+# Wait until apiserver and calico are Available. 
+for serv in calico apiserver
+do
+  printf "Waiting on %s: " "${serv}"
+  tigerastatus "${serv}"
   printf "Available\n"
 done
 
@@ -100,20 +138,10 @@ kubectl apply -f calico_enterprise/calico-enterprise-license.yaml
 
 # Wait for all components to become available
 printf "Waiting on all components: "
-AVAIL=False
-until [ ${AVAIL} == "True" ]
+
+for serv in monitor log-storage compliance intrusion-detection log-collector manager
 do
-  for condition in $(kubectl get tigerastatus --no-headers | sort -rk2 | awk '{print$2}')
-  do
-    if [ "${condition}" != "True" ]
-    then
-      sleep 2
-      AVAIL=False
-      break
-    else
-      AVAIL=True
-    fi
-  done
+  tigerastatus "${serv}"
 done
 
 printf "Available\n"
@@ -125,8 +153,8 @@ kubectl apply -f https://docs.tigera.io/manifests/tigera-policies.yaml
 kubectl create sa admin -n default --dry-run=client -o yaml | kubectl apply -f -
 kubectl create clusterrolebinding admin-access --clusterrole tigera-network-admin\
   --serviceaccount default:admin --dry-run=client -o yaml | kubectl apply -f -
-kubectl get secret "$(kubectl get serviceaccount admin -o jsonpath='{range .secrets[*]}{.name}{"\n"}{end}' | \
-  grep token)" -o go-template='{{.data.token | base64decode}}' && echo
+kubectl get secret "$(kubectl get serviceaccount admin -o jsonpath='{range .secrets[*]}{.name}{"\n"}{end}' | grep token)" \
+  -o go-template='{{.data.token | base64decode}}' && echo
 
 printf "\nVisit https://localhost:9443/ to login to the Calico Enterprise UI with token above.\n\n"
 
