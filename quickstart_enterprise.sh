45959733353f2e8b5f617919a6746e4f37aa6c65
Justin Cook <jhcook@secnix.com>
2022-05-03 09:35:20 +0100

    add nginx ingress to and ingress-dns if not on minikube

--- quickstart_enterprise.sh
+++ quickstart_enterprise.sh
@@ -4,7 +4,12 @@
 # Afterward, configuring Prometheus and Grafana and installing Boutique neeed
 # completed separately as they exit with runtime.
 #
+# References:
+#  * https://kubernetes.github.io/ingress-nginx/deploy/
+#
 # Author: Justin Cook
+
+set -o errexit
 
 # shellcheck source=/dev/null
 . env.sh
@@ -12,7 +17,25 @@
 trap "exit" INT
 
 # Configure Minikube
-bash setup_k8s.sh
+#bash setup_k8s.sh
+
+# Configure Kind
+#colima start --cpu 6 --memory 28 --disk 100 --runtime containerd
+#bash kind/configure_colima.sh
+#kind create cluster --config kind/calico_cluster.yaml
+
+# Install Calico CNI
+bash install_calico.sh
+
+# Install NGINX Ingress Controller
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.2/deploy/static/provider/cloud/deploy.yaml
+#kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=240s
+
+# Add ingress-dns if not on Minikube
+#kubectl apply -f kind/ingress-dns-pod.yaml
 
 # Install Rancher
 bash install_rancher.sh
