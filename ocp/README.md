78917687ddd6342210baf6ac8281f3c2ce7a5c36
Justin Cook <jhcook@secnix.com>
2022-08-11 19:11:12 +0100

    setup and configure ocp with user monitoring

---
+++ ocp/README.md
@@ -0,0 +1,13 @@
+# OpenShift Container Platform
+
+This module configures OpenShift Container Platform for use with the
+integrations and use cases in this repository.
+
+## Operations
+
+* Enable monitoring and allow user-defined project configuration
+* Configure restricted SCC with additional permissions
+
+## References
+
+* [Enabling monitoring for user-defined projects](https://docs.openshift.com/container-platform/4.10/monitoring/enabling-monitoring-for-user-defined-projects.html)
---
+++ ocp/setup_ocp.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+#
+# Copyright 2022 Justin Cook
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Setup OpenShift Local
+#
+# Requires: OpenShift Local installed and CRC running
+#
+# Author: Justin Cook
+
+set -o errexit nounset
+set -x
+
+# shellcheck source=/dev/null
+. env.sh
+
+crc setup
+crc config set cpus 6
+crc config set memory 30208
+crc config set disk-size 100
+crc config set enable-cluster-monitoring true
+
+if [ -n "${http_proxy-}" ]
+then
+  crc config set http-proxy "${http_proxy}"
+fi
+
+if [ -n "${https_proxy-}" ]
+then
+  crc config set https-proxy "${https_proxy}"
+fi
+
+if [ -n "${no_proxy-}" ]
+then
+  crc config set no-proxy "${no_proxy}"
+fi
+
+crc start
+
+# Create the namspace
+oc new-project "${PROJECT_NAMESPACE}" \
+  --description="This is the Google Boutique microservices demo" \
+  --display-name="Online Boutique"
+
+# Enable cluster monitoring of user namespaces
+kubectl apply -f ocp/cluster-monitoring-config.yaml
+
+# We are recreating the registries config file and restarting associated
+# services at the same time we are adding the integrated registry as insecure.
+#shellcheck disable=SC2034
+SSH_COM=$(paste -s -d ' ' - << __EOF__
+ssh
+-i ~/.crc/machines/crc/id_ecdsa
+-o StrictHostKeyChecking=no
+core@$(crc ip)
+-p2222
+__EOF__
+)
+
+#cat << __EOF__ | ${SSH_COM} "sudo bash -c \"cat - > /etc/containers/registries.conf\""
+#unqualified-search-registries = ['registry.access.redhat.com', 'docker.io', 'image-registry.openshift-image-registry.svc:5000']
+#prefix = ""
+
+#[[registry]]
+#  location = "image-registry.openshift-image-registry.svc:5000"
+#  insecure = true
+#  blocked = false
+#  mirror-by-digest-only = false
+#  prefix = ""
+#__EOF__
+
+#${SSH_COM} "sudo systemctl restart crio"
+#${SSH_COM} "sudo systemctl restart kubelet"
+:
