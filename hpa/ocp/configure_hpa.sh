607a72f32d3f5b767cd719607b86225b4e1e9c9d
Justin Cook <jhcook@secnix.com>
2022-08-16 16:50:46 +0100

    switch to oc, change dockerfile path, and add scc

--- hpa/ocp/configure_hpa.sh
+++ hpa/ocp/configure_hpa.sh
@@ -22,12 +22,123 @@
 #
 # Configure OpenShift for use with HPA
 #
+# Requires: oc
+#
 # Author: Justin Cook
 
 set -o errexit nounset
 
 # shellcheck source=/dev/null
 . env.sh
+
+# Install the OpenShift custom metrics autoscaler
+oc create namespace openshift-keda --dry-run=client -o yaml | \
+  oc apply -f -
+for file in openshift-keda-operator-group openshift-keda-subscription \
+            custom-metrics-autoscaler
+do
+  oc create -f hpa/ocp/${file}.yaml -n openshift-keda --dry-run=client -o yaml | 
+    oc apply -f -
+done
+
+# Wait for all the deployments to become available
+for deploy in $(oc get deploy -n openshift-keda -o name)
+do
+  oc rollout status "${deploy}" -n openshift-keda
+done
+
+oc create -f hpa/ocp/openshift-keda-controller.yaml -n openshift-keda \
+  --dry-run=client -o yaml | oc apply -f -
+
+# Wait for kedacontrollers.keda.sh to become available
+oc wait --for condition=established --timeout=60s crd/kedacontrollers.keda.sh
+
+# Create a Security Context Constraints (SCC) allowing the appropriate Linux
+# capabilities for conntrack-network-init and tcp-exporter containers.
+
+oc apply -f - << __EOF__
+apiVersion: security.openshift.io/v1
+kind: SecurityContextConstraints
+metadata:
+  annotations:
+    kubernetes.io/description: provides all features of the restricted SCC
+      but allows users to run with any UID, any GID, and allows escalated
+      network and discretionary access control privileges.
+  name: net-dac-cap-anyuid
+allowHostDirVolumePlugin: false
+allowHostIPC: false
+allowHostNetwork: false
+allowHostPID: false
+allowHostPorts: false
+allowPrivilegeEscalation: true
+allowPrivilegedContainer: false
+allowedCapabilities:
+  - NET_BIND_SERVICE
+  - NET_ADMIN
+  - NET_RAW
+  - DAC_READ_SEARCH
+  - DAC_OVERRIDE
+defaultAddCapabilities: null
+fsGroup:
+  type: RunAsAny
+groups:
+- system:authenticated
+priority: 10
+readOnlyRootFilesystem: false
+runAsUser:
+  type: RunAsAny
+seLinuxContext:
+  type: RunAsAny
+supplementalGroups:
+  type: RunAsAny
+users: []
+volumes:
+- configMap
+- downwardAPI
+- emptyDir
+- persistentVolumeClaim
+- projected
+- secret
+__EOF__
+
+# Create a role allowing the use of the net-dac-cap-restricted SCC, a service
+# account, and a role binding the service account to the role. 
+
+oc apply -f - -n "${PROJECT_NAMESPACE}" << __EOF__
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: net-dac-cap-anyuid
+  namespace: "${PROJECT_NAMESPACE}"
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: net-dac-cap-anyuid
+  namespace: "${PROJECT_NAMESPACE}"
+rules:
+- apiGroups:
+  - security.openshift.io 
+  resourceNames:
+  - net-dac-cap-anyuid
+  resources:
+  - securitycontextconstraints 
+  verbs: 
+  - use
+---
+  apiVersion: rbac.authorization.k8s.io/v1
+  kind: RoleBinding
+  metadata:
+    name: net-dac-cap-anyuid
+    namespace: "${PROJECT_NAMESPACE}"
+  subjects:
+  - kind: ServiceAccount
+    name: net-dac-cap-anyuid
+  roleRef:
+    kind: Role
+    name: net-dac-cap-anyuid
+    apiGroup: rbac.authorization.k8s.io
+__EOF__
 
 # Iterate through the `hpa/sidecar` subdirectories which should be container
 # builds using Dockerfile. In each of these directories, set the path as the
@@ -39,7 +150,7 @@ do
   if [ -d "${i}" ]
   then
     # Create the ImageStream
-    kubectl apply -f - << __EOF__
+    oc apply -f - << __EOF__
 kind: ImageStream
 apiVersion: image.openshift.io/v1
 metadata:
@@ -53,7 +164,7 @@ spec:
 __EOF__
 
     # Create the BuildConfig
-    kubectl apply -f - << __EOF__
+    oc apply -f - << __EOF__
 apiVersion: build.openshift.io/v1
 kind: BuildConfig
 metadata:
@@ -70,7 +181,7 @@ spec:
   strategy:
     type: Docker
     dockerStrategy:
-      dockerfilePath: Dockerfile
+      dockerfilePath: ocp/Dockerfile
   output:
     to:
       kind: ImageStreamTag
@@ -87,4 +198,5 @@ __EOF__
   fi
 done
 
-oc set image-lookup imagestream --list
+# Create an Ingress for the Boutique®
+oc apply -f hpa/frontend-ingress.yaml -n "${PROJECT_NAMESPACE}"
